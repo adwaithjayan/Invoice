707e8106a3c77c16f8ca81e1a30187a8233e19dd
Adwaith jayan <aj007boss@gmail.com>
2024-12-15 00:05:55 +0530

    update

--- actions/action.ts
+++ actions/action.ts
@@ -83,7 +83,7 @@ export async function createInvoice(prevState:any,formData:FormData) {
                         dateStyle:'long'
                   }).format(new Date(submission.value.dueDate)),
                   "totalAmount": formatCurrency({amount:submission.value.total,currency:submission.value.currency as any}),
-                  "invoiceLink":`http://localhost:3000/api/invoice/${data.id}`,
+                  "invoiceLink":process.env.NODE_ENV !== 'production'?`http://localhost:3000/api/invoice/${data.id}`:`https://invoice-one-kappa.vercel.app/api/invoice/${data.id}`,
             }
       })
 
@@ -143,7 +143,7 @@ export async function editInvoice(prevState:any,formData:FormData) {
                         dateStyle:'long'
                   }).format(new Date(submission.value.dueDate)),
                   "totalAmount": formatCurrency({amount:submission.value.total,currency:submission.value.currency as any}),
-                  "invoiceLink":`http://localhost:3000/api/invoice/${data.id}`,
+                  "invoiceLink":process.env.NODE_ENV !=='production'?`http://localhost:3000/api/invoice/${data.id}`:`https://invoice-one-kappa.vercel.app/api/invoice/${data.id}`,
             }
       })
       return redirect('/dashboard/invoices');
